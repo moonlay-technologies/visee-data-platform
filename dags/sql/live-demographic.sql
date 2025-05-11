46397c5a659186f438d493b9e66952f68ea5a396
maulanardy <maulanardy.dev@gmail.com>
2025-05-11 09:32:02 +0700

    update if already exist

--- dags/sql/live-demographic.sql
+++ dags/sql/live-demographic.sql
@@ -1,8 +1,8 @@
-insert into live_demographic (record_time, record_time_end, avg_dwell_time, client_id, zone_id, object_id, session_id, gender, emotion, age, store_id, activity, attributes, detection_type)
+INSERT INTO live_demographic (record_time, record_time_end, avg_dwell_time, client_id, zone_id, object_id, session_id, gender, emotion, age, store_id, activity, attributes, detection_type)
 SELECT 
-	max(recording_time) as recording_time,
-	max(recording_end) as recording_time_end,
-	max(recording_end) - max(recording_time) as dwell_time,
+    max(recording_time) AS recording_time,
+    max(recording_end) AS recording_time_end,
+    max(recording_end) - max(recording_time) AS dwell_time,
     vr.client_id,
     vr.zone_id,
     vr.object_id,
@@ -11,8 +11,8 @@ SELECT
     vr.emotion,
     vr.age,
     vr.store_id,
-    max(vr.activity) as activity,
-    max(vr.attributes) as attributes,
+    max(vr.activity) AS activity,
+    max(vr.attributes) AS attributes,
     vr.detection_type
 FROM viseetor_raw vr
 GROUP BY 
@@ -25,3 +25,7 @@ GROUP BY
     vr.age,
     vr.store_id,
     vr.detection_type
+ON CONFLICT (client_id, zone_id, object_id, session_id, store_id, detection_type) 
+DO UPDATE SET 
+    record_time_end = EXCLUDED.record_time_end,
+    avg_dwell_time = EXCLUDED.avg_dwell_time;
