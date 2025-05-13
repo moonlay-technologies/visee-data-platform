44834ca5fe09810353014628acb6d4ba67569c13
maulanardy <maulanardy.dev@gmail.com>
2025-05-13 11:24:03 +0700

    update the query

--- dags/sql/live-demographic.sql
+++ dags/sql/live-demographic.sql
@@ -1,30 +1,43 @@
+WITH ranked_rows AS (
+    SELECT 
+        recording_time,
+        recording_end,
+        recording_end - recording_time AS dwell_time,
+        vr.client_id,
+        vr.zone_id,
+        vr.object_id,
+        vr.session_id::uuid,
+        vr.gender,
+        vr.emotion,
+        vr.age,
+        vr.store_id,
+        vr.activity,
+        vr.attributes,
+        vr.detection_type,
+        ROW_NUMBER() OVER (
+            PARTITION BY vr.client_id, vr.zone_id, vr.object_id, vr.session_id, vr.store_id
+            ORDER BY ttl DESC
+        ) AS row_num
+    FROM viseetor_raw vr
+)
 INSERT INTO live_demographic (record_time, record_time_end, avg_dwell_time, client_id, zone_id, object_id, session_id, gender, emotion, age, store_id, activity, attributes, detection_type)
 SELECT 
-    max(recording_time) AS recording_time,
-    max(recording_end) AS recording_time_end,
-    max(recording_end) - max(recording_time) AS dwell_time,
-    vr.client_id,
-    vr.zone_id,
-    vr.object_id,
-    vr.session_id::uuid,
-    vr.gender,
-    vr.emotion,
-    vr.age,
-    vr.store_id,
-    max(vr.activity) AS activity,
-    max(vr.attributes) AS attributes,
-    vr.detection_type
-FROM viseetor_raw vr
-GROUP BY 
-    vr.client_id, 
-    vr.zone_id, 
-    vr.object_id, 
-    vr.session_id,
-    vr.gender,
-    vr.emotion,
-    vr.age,
-    vr.store_id,
-    vr.detection_type
+    recording_time,
+    recording_end,
+    dwell_time,
+    client_id,
+    zone_id,
+    object_id,
+    session_id,
+    gender,
+    emotion,
+    age,
+    store_id,
+    activity,
+    attributes,
+    detection_type
+FROM ranked_rows
+WHERE row_num = 1
 ON CONFLICT (client_id, zone_id, object_id, session_id, store_id) 
 DO UPDATE SET 
     record_time_end = EXCLUDED.record_time_end,
